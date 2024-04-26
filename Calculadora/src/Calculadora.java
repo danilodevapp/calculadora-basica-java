115fdf90a718a9b543edf46afcdb4ad2e0a52c0c
danilodevapp <danilodevapp@gmail.com>
2024-04-26 13:54:37 -0300

    Calculadora documentada

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,38 +1,54 @@
+//Esse programa se trata de uma aplicação simples, para calcular a soma, 
+//subtração, multiplicação e divisão de dois números.
+
 import java.util.Scanner;
 import java.util.Locale;
 
 public class Calculadora {
     public static void main(String[] args) {        
     
+    //Criado o objeto scanner para recebimento das variáveis...
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    //Instanciada a Classe Calcular para permitir acesso aos dados da classe
     Calcular calcular = new Calcular();
 
+    //Recebe o primeiro valor do usuário para ser calculado
     System.out.print("Digite o primeiro número: ");
     calcular.Numero1 = scanner.nextDouble();    
     System.out.println("");
 
+    //Recebe o segundo valor do usuário para ser calculado
     System.out.print("Digite o segundo número: ");
     calcular.Numero2 = scanner.nextDouble();
     System.out.println("");
 
     System.out.println("Efetuando os cálculos...");
     System.out.println("");
-
+    
+    //Cria variável soma para receber o valor da função Somar na Classe Calcular
     double soma = calcular.Somar(0, 0);
+    //Cria variável multiplicacao para receber o valor da função Multiplicar na Classe Calcular    
     double multiplicacao = calcular.Multiplicar(0, 0);
+    //Cria a variável subtracao para receber o valor da função Subtrair na Classe Calcular
     double subtracao = calcular.Subtrair(0, 0);
+    //Cria a variável divisao para receber o valor da função Dividir na Classe Calcular
     double divisao = calcular.Dividir(0, 0);
 
+    //Dispara o método Somar na Classe Calcular
     calcular.Somar(0, 0);
+    //Imprime o resultado da soma dos números
     System.out.println("O resultado da soma é: " + soma);
-
+    //Dispara o método Subtrair na Classe Calcular
     calcular.Subtrair(0, 0);
+    //Imprimeo o resultado da subtração dos números
     System.out.println("O resultado da subtração é: " + subtracao);
-
+    //Dispara o método Multiplicar na Classe Calcular
     calcular.Multiplicar(0, 0);
+    //Imprime o resultado da multiplicação dos números
     System.out.println("O resultado da multiplicação é: " + multiplicacao);
-
+    //Dispara o método Dividir na Classe Calcular
     calcular.Dividir(0, 0);
+    //Imprime o resultado da divisão dos números
     System.out.println("O resultado da divisão é: " + divisao);
 
 }
--- Calculadora/src/Calcular.java
+++ Calculadora/src/Calcular.java
@@ -2,24 +2,28 @@ public class Calcular {
 
     public double Numero1, Numero2;
 
+    //Método que permite a soma de dois números passados por parâmetros com retorno double
     public double Somar (double numero1, double numero2){
         numero1 = Numero1;
         numero2 = Numero2;
         return numero1 + numero2;
     }
 
+    //Método que permite a subtração de dois números passados por parâmetros com retorno double
     public double Subtrair (double numero1, double numero2){
         numero1 = Numero1;
         numero2 = Numero2;
         return numero1 - numero2;
     }
 
+    //Método que permite a multiplicação de dois números passados por parâmetros com retorno double
     public double Multiplicar (double numero1, double numero2){
         numero1 = Numero1;
         numero2 = Numero2;
         return numero1 * numero2;
     }
 
+    //Método que permite a divisão de dois números passados por parâmetros com retorno double
     public double Dividir (double numero1, double numero2){
         numero1 = Numero1;
         numero2 = Numero2;
